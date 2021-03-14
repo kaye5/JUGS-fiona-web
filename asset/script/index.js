fd235f07518f79eea0e97451772f0994857a7a41
kaye <kevinyusyus@gmail.com>
2021-03-14 10:09:20 +0700

    lowongan-detail

--- asset/script/index.js
+++ asset/script/index.js
@@ -1,9 +1,20 @@
-var hoverImage = ['img2.jpg','img2.jpg','img2.jpg']
-var originalImage = ['img1.jpg','img1.jpg','img1.jpg']
-function content02ImageHover (id) {    
-    $('#content-02-'+id).attr('src','asset/img/'+hoverImage[parseInt(id)-1])
+var hoverImage = ['img2.jpg', 'img2.jpg', 'img2.jpg']
+var originalImage = ['img1.jpg', 'img1.jpg', 'img1.jpg']
+function content02ImageHover(id) {
+  $('#content-02-' + id).attr(
+    'src',
+    'asset/img/' + hoverImage[parseInt(id) - 1]
+  )
 }
 
-function content02ImageOut (id){
-    $('#content-02-'+id).attr('src','asset/img/'+originalImage[parseInt(id)-1])
+function content02ImageOut(id) {
+  $('#content-02-' + id).attr(
+    'src',
+    'asset/img/' + originalImage[parseInt(id) - 1]
+  )
+}
+
+function copyLink() {
+  var value = document.getElementById('copy-link-input').value
+  navigator.clipboard.writeText(value)
 }
